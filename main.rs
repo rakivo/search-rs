61dc9162a7c2e405013db74ba0dc43d25dd33eb2
Mark Tyrkba <awfulmarco@gmail.com>
2024-10-10 18:33:15 +0300

    use Zozin's way of indexing things, speed up the whole thing significantly

--- main.rs
+++ main.rs
@@ -17,40 +17,14 @@ use lopdf::{Document, Object};
 use foldhash::fast::RandomState;
 use xml::reader::{EventReader, XmlEvent};
 
-type Contents = Vec::<(PathBuf, String)>;
-type TfIdf<'a> = HashMap::<&'a str, f32>;
-type Index<'a> = HashMap::<&'a str, usize>;
-type Indexes<'a> = HashMap::<&'a PathBuf, Index<'a>>;
-type Rank<'a> = Vec::<(&'a str, f32)>;
-type PathRanks<'a> = Vec::<(&'a PathBuf, f32)>;
-type Ranks<'a> = HashMap::<&'a PathBuf, Rank<'a>>;
-
 const SPLIT_CHARACTERS: &[char] = &[' ', ',', '.'];
 
-const IGNORE: &[&str] = &[
-    "Length",
-    "BBox",
-    "FormType",
-    "Matrix",
-    "Type",
-    "XObject",
-    "Subtype",
-    "Filter",
-    "ColorSpace",
-    "Width",
-    "Height",
-    "BitsPerComponent",
-    "Length1",
-    "Length2",
-    "Length3",
-    "PTEX.FileName",
-    "PTEX.PageNumber",
-    "PTEX.InfoDict",
-    "FontDescriptor",
-    "ExtGState",
-    "MediaBox",
-    "Annot",
-];
+const IGNORE: &[&str] = &["Length", "BBox", "FormType", "Matrix", "Type", "XObject", "Subtype", "Filter", "ColorSpace", "Width", "Height", "BitsPerComponent", "Length1", "Length2", "Length3", "PTEX.FileName", "PTEX.PageNumber", "PTEX.InfoDict", "FontDescriptor", "ExtGState", "MediaBox", "Annot",];
+
+type Contents = Vec::<(PathBuf, String)>;
+type DocFreq<'a> = HashMap<&'a str, usize>;
+type TermFreq<'a> = HashMap<&'a str, usize>;
+type Ranks<'a> = Vec::<(&'a PathBuf, f32)>;
 
 pub struct DirRec {
     stack: VecDeque::<PathBuf>,
@@ -97,14 +71,16 @@ where
 }
 
 struct PdfText {
-    text: BTreeMap<u32, Vec::<String>>, // Key is page number
-    errors: Vec<String>,
+    // key is page number
+    text: BTreeMap<u32, Vec::<String>>,
+    errors: Vec::<String>
 }
 
 fn filter_func(object_id: (u32, u16), object: &mut Object) -> Option::<((u32, u16), Object)> {
     if IGNORE.contains(&object.type_name().unwrap_or_default()) {
         return None;
     }
+
     if let Ok(d) = object.as_dict_mut() {
         d.remove(b"Producer");
         d.remove(b"ModDate");
@@ -118,9 +94,11 @@ fn filter_func(object_id: (u32, u16), object: &mut Object) -> Option::<((u32, u1
             return None;
         }
     }
+
     Some((object_id, object.to_owned()))
 }
 
+#[inline(always)]
 fn load_pdf<P>(path: P) -> Result::<Document, IoError>
 where
     P: AsRef::<Path>
@@ -129,35 +107,36 @@ where
 }
 
 fn get_pdf_text(doc: &Document) -> Result::<PdfText, IoError> {
-    let mut pdf_text: PdfText = PdfText {
+    use std::sync::{Arc, Mutex};
+
+    let mut pdf_text = PdfText {
         text: BTreeMap::new(),
         errors: Vec::new(),
     };
 
-    let pages = doc.get_pages()
+    let pdf_text_am = Arc::new(Mutex::new(&mut pdf_text));
+
+    doc.get_pages()
         .into_par_iter()
-        .map(|(page_num, page_id)| {
-            let text = doc.extract_text(&[page_num]).map_err(|e| {
+        .map(|(npage, page_id)| {
+            let text = doc.extract_text(&[npage]).map_err(|e| {
                 IoError::new(IoErrorKind::Other,
-                             format!("could not to extract text from page {page_num} id={page_id:?}: {e:}"))
+                             format!("could not to extract text from page {npage} id={page_id:?}: {e:}"))
             })?;
 
-            Ok((page_num,
+            Ok((npage,
                 text.split('\n')
-                    .map(|s| s.trim_end().to_string())
-                    .collect::<Vec<String>>()))
-        }).collect::<Vec::<Result::<(u32, Vec::<String>), IoError>>>();
+                .map(|s| prepare_word(s).to_string())
+                .collect()))
 
-    for page in pages {
-        match page {
-            Ok((page_num, lines)) => {
-                pdf_text.text.insert(page_num, lines);
+        }).for_each(|page: Result::<_, IoError>| {
+            let mut pdf_text = unsafe { pdf_text_am.lock().unwrap_unchecked() };
+            match page {
+                Ok((npage, lines)) => { pdf_text.text.insert(npage, lines); },
+                Err(e) => pdf_text.errors.push(e.to_string()),
             }
-            Err(e) => {
-                pdf_text.errors.push(e.to_string());
-            }
-        }
-    }
+        });
+
     Ok(pdf_text)
 }
 
@@ -180,11 +159,13 @@ impl ParseFn for Pdf {
             let err = IoError::new(IoErrorKind::InvalidData, "doc is encrypted");
             return Err(err)
         }
+
         let text = get_pdf_text(&doc)?;
         if !text.errors.is_empty() {
             let err = IoError::new(IoErrorKind::InvalidData, "could not parse document as pdf");
             return Err(err)
         }
+
         let string = text.text.iter().map(|(_, text)| text.join(" ")).collect::<String>();
         Ok(string)
     }
@@ -211,7 +192,7 @@ impl ParseFn for Html {
     {
         let input = read_to_string(&file_path)?;
         let Ok(dom) = tl::parse(&input, ParserOptions::default()) else {
-            let err = std::io::Error::new(std::io::ErrorKind::InvalidData, "could not parse html");
+            let err = IoError::new(IoErrorKind::InvalidData, "could not parse html");
             return Err(err)
         };
         let parser = dom.parser();
@@ -243,10 +224,12 @@ impl ParseFn for Xml {
 
 #[inline]
 fn parse(file_path: &Path) -> IoResult::<String> {
-    let ext = file_path.extension()
-        .unwrap_or_default()
-        .to_str()
-        .unwrap();
+    let ext = unsafe {
+        file_path.extension()
+            .unwrap_or_default()
+            .to_str()
+            .unwrap_unchecked()
+    };
 
     match ext {
         "pdf" => Pdf::parse(file_path),
@@ -255,6 +238,13 @@ fn parse(file_path: &Path) -> IoResult::<String> {
         _ => Txt::parse(file_path),
     }
 }
+
+pub struct Doc<'a> {
+    tf: TermFreq<'a>,
+    count: usize,
+}
+
+type Docs<'a> = HashMap::<&'a PathBuf, Doc<'a>>;
 
 // trim and lowercase all the words but without copying
 #[inline]
@@ -271,93 +261,106 @@ fn prepare_word<'a>(word: &'a str) -> &'a str {
     }
 }
 
-#[inline]
-fn index_content<'a>(content: &'a str) -> Index<'a> {
-    content.split(SPLIT_CHARACTERS)
-        .filter(|s| s.chars().all(|c| c.is_alphabetic()))
-        .fold(Index::with_capacity_and_hasher(128, RandomState::default()),
-              |mut map, word| {
-                  *map.entry(prepare_word(word)).or_insert(0) += 1;
-                  map
-              })
-}
-
-#[inline(always)]
-fn tf(t: &str, d: &Index) -> f32 {
-    *d.get(t).unwrap_or(&1) as f32 / d.par_iter().map(|(_, f)| f).sum::<usize>() as f32
-}
-
-#[inline(always)]
-fn idf(t: &str, df: &Indexes) -> f32 {
-    let n = df.len() as f32;
-    let m = df.par_values().filter(|tf| tf.contains_key(t)).count() as f32;
-    (n / m).log10()
-}
-
-fn compute_ranks<'a>(contents: &'a Contents) -> Ranks<'a> {
-    let indexes = contents.par_iter()
-        .map(|(file_path, string)| (file_path, index_content(string)))
-        .collect::<Indexes>();
-    
-    indexes.par_iter().map(|(path, index)| {
-        let tf_idf = index.par_iter().map(|(term, _)| {
-            let tf = tf(term, index);
-            let idf = idf(term, &indexes);
-            (*term, tf*idf)
-        }).collect::<TfIdf>();
-
-        (*path, tf_idf)
-    }).map(|(path, tf_idf)| {
-        let mut stats = tf_idf.iter().collect::<Vec::<_>>();
-        stats.par_sort_unstable_by(|(_, a), (_, b)| unsafe {
-            b.partial_cmp(a).unwrap_unchecked()
+impl<'a> Doc<'a> {
+    pub fn new(content: &'a str) -> Self {
+        let (count, tf) = content.split(SPLIT_CHARACTERS).fold({
+            (0, TermFreq::with_capacity_and_hasher(128, RandomState::default()))
+        }, |(c, mut tf), word| {
+            let word = prepare_word(word);
+            if !word.is_empty() {
+                *tf.entry(word).or_insert(0) += 1;
+                (c + 1, tf)
+            } else {
+                (c, tf)
+            }
         });
 
-        let top = stats.into_iter().take(50).map(|(s, f)| (*s, *f)).collect();
-        (path, top)
-    }).collect()
-}
-
-fn rank_documents_by_term<'a>(terms: &str, ranks: &'a Ranks) -> PathRanks<'a> {
-   let search_terms = terms.split(SPLIT_CHARACTERS)
-        .map(|term| term.to_lowercase())
-        .collect::<Vec::<_>>();
-
-    let mut doc_ranks = ranks
-        .into_par_iter()
-        .map(|(path, rank)| {
-            let rank = search_terms.iter().map(|search_term| {
-                rank.iter()
-                    .find(|(term, _)| term.eq(search_term))
-                    .map_or(0.0, |(_, rank)| *rank)
-            }).sum();
-
-            (*path, rank)
-        }).collect::<PathRanks>();
-
-    doc_ranks.par_sort_unstable_by(|(_, a), (_, b)| unsafe {
-        b.partial_cmp(a).unwrap_unchecked()
-    });
-
-    doc_ranks
-}
-
-#[inline]
-#[allow(unused)]
-fn print_ranks(ranks: &Ranks) {
-    for (path, ranks) in ranks {
-        println!("{path:?}:");
-        for (term, tf_idf) in ranks {
-            println!("    {term} => {tf_idf}");
-        }
+        Doc { tf, count }
     }
 }
 
-#[inline]
-#[allow(unused)]
-fn print_path_ranks(ranks: &PathRanks) {
-    for (path, rank) in ranks.iter().filter(|(_, rank)| *rank != 0.0).take(5) {
-        println!("{path:?} => {rank}")
+pub struct Model<'a> {
+    pub docs: Docs<'a>,
+    pub df: DocFreq<'a>
+}
+
+impl<'a> Model<'a> {
+    pub fn new() -> Self {
+        Model {
+            docs: HashMap::with_capacity_and_hasher(32, RandomState::default()),
+            df: HashMap::with_capacity_and_hasher(128, RandomState::default())
+        }
+    }
+
+    pub fn search(&self, query: &str) -> Ranks {
+        let tokens = query.split(SPLIT_CHARACTERS).collect::<Vec<_>>();
+
+        let mut ranks = self.docs.par_iter().filter_map(|(path, doc)| {
+            let rank = tokens.iter().map(|token| {
+                Self::tf(token, doc)*self.idf(token)
+            }).sum::<f32>();
+
+            if !rank.is_nan() {
+                Some((*path, rank))
+            } else {
+                None
+            }
+        }).collect::<Vec::<_>>();
+
+        ranks.sort_unstable_by(|a, b| unsafe { b.1.partial_cmp(&a.1).unwrap_unchecked() });
+        ranks
+    }
+
+    pub fn add_document(&mut self, file_path: &'a PathBuf, content: &'a str) {
+        self.rm_document(&file_path);
+
+        let (count, tf) = content.split(SPLIT_CHARACTERS)
+            .filter(|s| s.chars().all(|c| c.is_alphanumeric()))
+            .fold({
+                (0, TermFreq::with_capacity_and_hasher(128, RandomState::default()))
+            }, |(c, mut tf), t| {
+                if let Some(f) = tf.get_mut(t) {
+                    *f += 1;
+                } else {
+                    tf.insert(t, 1);
+                } (c + 1, tf)
+            });
+
+        tf.keys().for_each(|t| {
+            if let Some(f) = self.df.get_mut(t) {
+                *f += 1;
+            } else {
+                self.df.insert(t, 1);
+            }
+        });
+
+        self.docs.insert(file_path, Doc {count, tf});
+    }
+
+    #[inline]
+    pub fn add_contents(&mut self, contents: &'a Contents) {
+        contents.iter().for_each(|(file_path, content)| {
+            self.add_document(file_path, content);
+        });
+    }
+
+    #[inline]
+    fn rm_document(&mut self, file_path: &PathBuf) {
+        if let Some(doc) = self.docs.remove(file_path) {
+            doc.tf.keys().for_each(|t| {
+                self.df.entry(t).and_modify(|f| *f -= 1);
+            });
+        }
+    }
+
+    #[inline(always)]
+    fn tf(t: &str, doc: &Doc) -> f32 {
+        *doc.tf.get(t).unwrap_or(&0) as f32 / doc.count as f32
+    }
+
+    #[inline(always)]
+    fn idf(&self, term: &str) -> f32 {
+        (self.docs.len() as f32 / *self.df.get(term).unwrap_or(&0) as f32).log10()
     }
 }
 
@@ -372,7 +375,7 @@ fn main() -> ExitCode {
     let start = Instant::now();
 
     let ref dir_path = args[1];
-    let term = args[2].to_lowercase();
+    let term = args[2..].join(" ").to_lowercase();
 
     let dir = DirRec::new(dir_path);
     let contents = dir.into_iter()
@@ -380,17 +383,14 @@ fn main() -> ExitCode {
         .filter_map(|e| parse(&e).ok().map(|r| (e, r)))
         .collect::<Contents>();
 
-    let ranks = compute_ranks(&contents);
-
-    // print_ranks(&ranks);
+    let mut model = Model::new();
+    model.add_contents(&contents);
+    let _results = model.search(&term);
 
     #[cfg(feature = "dbg")] {
         let end = start.elapsed().as_millis();
         println!("indexing took: {end} millis");
     }
-
-    let path_ranks = rank_documents_by_term(&term, &ranks);
-    print_path_ranks(&path_ranks);
 
     return ExitCode::SUCCESS
 }
